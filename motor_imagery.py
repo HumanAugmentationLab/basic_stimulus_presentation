918309a64ccac7ad00413594e2ba2db728423e2b
snielsen221b <sophianielsen221b@gmail.com>
2017-03-01 15:14:45 -0500

    creation of window and stimuli, pt1

--- motor_imagery.py
+++ motor_imagery.py
@@ -2,6 +2,7 @@
 # CUES: acoustic stimuli (left/right) duration 1.5-8s
 
 import os
+from psychopy import data
 
 # ------------------------------------
 # EXPERIMENT SETTINGS
@@ -10,7 +11,7 @@ import os
 datapath = 'data'                    # directory to save data
 impath = 'images'                    # directory where images can be found
 imlist = ['left', 'right', 'cross']  # image names without suffixes
-suffix = '.png'                         # suffix for images
+suffix = '.png'                      # suffix for images
 scrnsize = (1200, 800)               # screen size in pixels
 timelimit = 4                        # image freezing time in seconds
 changetime = .5                      # image changing time in seconds
@@ -30,6 +31,7 @@ exp_info = {}
 # Check for datapath directory
 if not os.path.isdir(datapath):
     os.makedirs(datapath)
+
 # Create a unique filename for the experiment data
 data_fname = exp_info['participant'] + '_' + exp_info['date']
 data_fname = os.path.join(datapath, data_fname)
@@ -41,4 +43,33 @@ data_fname = os.path.join(datapath, data_fname)
 # Check if all images exist
 for im in imlist:
     im_fname = os.path.join(impath, im+sfx)
-# Randomize the image order
+    if not os.path.exists(im_fname):
+        raise Exception('Image files not fond in image folder: ' + str(im))
+
+# TODO: find a way to define a set image order
+
+# ----------------------------------------------
+# CREATION OF WINDOW AND STIMULI
+# ----------------------------------------------
+
+# Open a window
+
+# Define trial start text
+text = "Press spacebar to start the trial"
+
+# Define the bitmap stimuli (contents can still change)
+# Define a bubble (position and size can still change)
+
+# ----------------------------------------------
+# DEFINE A TRIAL SEQUENCE
+# ----------------------------------------------
+
+# Define a list of trials with their properties:
+#   - Which image (without the suffix)
+#   - Which orientation
+stim_order = []
+for im in imlist:
+    stim_order.append({'im': im, 'ori': })
+
+trials = data.TrialHandler(stim_order, nReps=1, extraInfo=exp_info,
+                           method='sequential', originPath=datapath)
